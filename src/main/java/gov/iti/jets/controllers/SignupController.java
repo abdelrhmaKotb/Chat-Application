82abe8fdec059f3b781bbfe031cd7f6de31bd993
Esraa Abdelnaser <esraaabdalnaser@gmail.com>
2023-01-23 23:53:32 +0200

    Add Validation on Date-of-Birth

--- src/main/java/gov/iti/jets/controllers/SignupController.java
+++ src/main/java/gov/iti/jets/controllers/SignupController.java
@@ -3,8 +3,11 @@ package gov.iti.jets.controllers;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.ResourceBundle;
 import java.net.URL;
+import java.text.ParseException;
 import java.util.regex.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -13,6 +16,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+
 
 public class SignupController implements Initializable {
 
@@ -66,6 +70,7 @@ public class SignupController implements Initializable {
     void clickBtnSignup(ActionEvent event) {
         validatePassword();
         confirmPass();
+        validateDOB();
 
     }
 
@@ -95,6 +100,23 @@ public class SignupController implements Initializable {
             System.out.println("Wrong pass" + txtConfirmPassword.getText());
         } else {
             System.out.println(txtConfirmPassword.getText());
+        }
+    }
+
+    public void validateDOB() {
+        SimpleDateFormat sdfrmt = new SimpleDateFormat("MM/dd/yyyy");
+        sdfrmt.setLenient(false);
+
+        try {
+            System.out.println(datepickerDateOfBirth.getValue().getMonthValue() + "/"
+                    + datepickerDateOfBirth.getValue().getDayOfMonth() + "/"
+                    + datepickerDateOfBirth.getValue().getYear());
+            Date javaDate = sdfrmt.parse(datepickerDateOfBirth.getValue().getMonthValue() + "/"
+                    + datepickerDateOfBirth.getValue().getDayOfMonth() + "/"
+                    + datepickerDateOfBirth.getValue().getYear());
+            System.out.println(datepickerDateOfBirth + " is valid date format");
+        } catch (ParseException e) {
+            System.out.println(datepickerDateOfBirth + " is Invalid Date format");
         }
     }
 
