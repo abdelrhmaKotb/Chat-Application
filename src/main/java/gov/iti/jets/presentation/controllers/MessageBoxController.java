2c5e059ad26708fa4781aeebe82e4171c22a12c9
omniayman <58345026+omniayman@users.noreply.github.com>
2023-01-28 01:43:07 +0200

    Message Box controller

---
+++ src/main/java/gov/iti/jets/presentation/controllers/MessageBoxController.java
@@ -0,0 +1,50 @@
+package gov.iti.jets.presentation.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class MessageBoxController {
+    @FXML
+    private ImageView receiverImgView;
+    @FXML
+    private Label receiverNameLabel;
+    @FXML
+    private ChoiceBox fontStyleChooser;
+    @FXML
+    private ChoiceBox fontSizeChooser;
+    @FXML
+    private ColorPicker fontColorPicker;
+    @FXML
+    private ColorPicker backgroundColorPicker;
+    @FXML
+    private Button boldBtn;
+    @FXML
+    private Button italicBtn;
+    @FXML
+    private Button underlineBtn;
+    @FXML
+    private ImageView attachImgView;
+    @FXML
+    private ImageView sendImgView;
+    @FXML
+    private RadioButton chatbotRadioBtn;
+
+    @FXML
+    private void clickSendImgView(MouseEvent event) {
+    }
+    @FXML
+    private void clickAttachImgView(MouseEvent event) {
+    }
+    
+
+
+    
+
+    
+}
