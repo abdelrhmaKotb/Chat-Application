fa5cbd0f7af4407c84f44ff66fca356edb884e76
Esraa Abdelnaser <esraaabdalnaser@gmail.com>
2023-01-28 16:21:38 +0200

    Add create group btn and hoven on buttons

--- src/main/java/gov/iti/jets/presentation/controllers/ChatController.java
+++ src/main/java/gov/iti/jets/presentation/controllers/ChatController.java
@@ -5,6 +5,7 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 
@@ -25,12 +26,19 @@ public class ChatController implements Initializable {
     @FXML
     private ImageView signOutBtn;
     @FXML
+    private ImageView AddgroupBtn;
+    @FXML
     private ListView<?> contactList;
 
-   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-       
+        Tooltip.install(contactBtn, new Tooltip("Contacts"));
+        Tooltip.install(addContactBtn, new Tooltip("Add Contacts"));
+        Tooltip.install(groupBtn, new Tooltip("Groups"));
+        Tooltip.install(notificationBtn, new Tooltip("Notifications"));
+        Tooltip.install(editProfileBtn, new Tooltip("Edit Profile"));
+        Tooltip.install(signOutBtn, new Tooltip("Sign out"));
+        Tooltip.install(AddgroupBtn, new Tooltip("Add Group"));
     }    
 
     @FXML
@@ -56,5 +64,8 @@ public class ChatController implements Initializable {
     @FXML
     private void clickSignOutBtn(MouseEvent event) {
     }
-    
+
+    @FXML
+    private void clickAddGroupBtn(MouseEvent event) {
+    }
 }
