731a235c8df5977789e86b4403f44ad36d8b9d0c
Esraa Abdelnaser <esraaabdalnaser@gmail.com>
2023-02-12 21:35:56 +0200

    Update Notification GUI

--- Chat-Application-Client/src/main/java/gov/iti/jets/presentation/controllers/NotificationController.java
+++ Chat-Application-Client/src/main/java/gov/iti/jets/presentation/controllers/NotificationController.java
@@ -4,28 +4,66 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
-
+import javafx.scene.text.TextAlignment;
+import javafx.scene.layout.HBox;
 
 public class NotificationController implements Initializable {
 
     @FXML
-    private VBox contacts;
+    private VBox vBoxRoot;
     @FXML
     private Text notificationsLbl;
     @FXML
-    private VBox notificationsVBox;
+    private ListView<String> listOfNotifications;
 
-   
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        for(int i = 0; i<20; i++){
-            Label lbl = new Label("Notificatoins"+i);
-            notificationsVBox.getChildren().add(lbl);
+        for (int i = 0; i < 3; i++) {
+            addInListOfNotifications( "notiiiiiiiiiiiiiiii" + i);
         }
-        
-    }    
-    
+    }
+
+    public void addInListOfNotifications(String notification) {
+
+        listOfNotifications.getItems().add(0, notification);
+        listOfNotifications.setCellFactory(lv -> {
+            ListCell<String> cell = new ListCell<String>() {
+
+                private HBox hbox = new HBox();
+                private Label label = new Label();
+                private ImageView imageView = new ImageView(getClass().getResource("/images/round.png").toString());
+                {
+                    setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+                    imageView.setFitHeight(10);
+                    imageView.setFitWidth(10);
+                    imageView.setTranslateY(4);
+                    hbox.getChildren().addAll(imageView, label);
+                }
+
+                @Override
+                protected void updateItem(String item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (empty) {
+                        setGraphic(null);
+                    } else {
+                        label.setText(item);
+                        label.setWrapText(true);
+                        label.setTextAlignment(TextAlignment.JUSTIFY);
+                        label.setMaxWidth(150);
+                        hbox.setMaxWidth(150);
+                        setGraphic(hbox);
+                    }
+                }
+            };
+            return cell;
+        });
+    }
+
 }
