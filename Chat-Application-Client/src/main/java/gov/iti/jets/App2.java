a7ed6d015729e4d5d23194939e8ac0fa702fc6d2
Abdelrhman Kotb <87202131+abdelrhmaKotb@users.noreply.github.com>
2023-02-15 06:46:46 +0200

    .

---
+++ Chat-Application-Client/src/main/java/gov/iti/jets/App2.java
@@ -0,0 +1,54 @@
+package gov.iti.jets;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import gov.iti.jets.business.helper.StageCoordinator;
+import gov.iti.jets.business.rmi.RMIConnection;
+import gov.iti.jets.interfaces.Client;
+// import gov.iti.jets.presentation.utils.chatBot;
+
+
+public class App2 extends Application {
+
+    static Client client;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        StageCoordinator coordinator = StageCoordinator.getInstance();
+        coordinator.setStage(primaryStage);
+        coordinator.moveToLogin();
+        primaryStage.setMinWidth(1315);
+        primaryStage.setMaxHeight(915);
+        primaryStage.show();
+
+    }
+
+    public static void main(String[] args) {
+        Application.launch(args);
+
+    }
+
+    @Override
+    public void init() throws Exception {
+        try {
+
+            RMIConnection rmi = RMIConnection.getInstance();
+            rmi.connect("localhost");
+            // client = new ClientImpl();
+
+            // Server serverServices = (Server) Naming.lookup("rmi://localhost:14785/serverService");
+
+            // serverServices.register(client);
+
+            // serverServices.sayHello();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void stop() throws Exception {
+        RMIConnection.getServerServive().unregister(RMIConnection.getInstance().getCurrentClientConnection());
+    }
+}
--- Chat-Application-Server/src/main/java/gov/iti/jets/App.java
+++ Chat-Application-Server/src/main/java/gov/iti/jets/App.java
@@ -1,41 +1,8 @@
 package gov.iti.jets;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
-import gov.iti.jets.business.rmi.RMIConnection;
-import gov.iti.jets.business.rmi.ServerImpl;
-import gov.iti.jets.dto.CountryDto;
-import gov.iti.jets.interfaces.Server;
-import gov.iti.jets.persistence.dao.UserImpl;
-import gov.iti.jets.persistence.dao.countryDaoImpl;
-import gov.iti.jets.persistence.entities.User;
-import gov.iti.jets.persistence.utils.DBConnecttion;
-import gov.iti.jets.persistence.utils.chatBot;
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import gov.iti.jets.persistence.utils.chatBot;
-
-public class App extends Application {
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-
-        Parent root = FXMLLoader.load(getClass().getResource("/views/MainPanelView.fxml"));
-        DBConnecttion.getConnection();
-        Scene sc = new Scene(root, 800, 600);
-        primaryStage.setScene(sc);
-        primaryStage.setTitle("Admin Panel");
-        primaryStage.show();
-
-    }
-
-    public static void main(String[] args) {
-        RMIConnection rmi = RMIConnection.getInstance();
-        DBConnecttion.getConnection();
-        rmi.connect();
-        Application.launch(args);
+public class App {
+    public static void main(final String[] args) {
+        App2.main(args);
     }
 }
+
