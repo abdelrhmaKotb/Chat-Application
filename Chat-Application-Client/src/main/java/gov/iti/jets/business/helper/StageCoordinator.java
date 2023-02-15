ad72f022129833713bd337c40945c2007395bd48
Abdelrhman Kotb <87202131+abdelrhmaKotb@users.noreply.github.com>
2023-02-15 20:04:06 +0200

    fds

--- Chat-Application-Client/src/main/java/gov/iti/jets/business/helper/StageCoordinator.java
+++ Chat-Application-Client/src/main/java/gov/iti/jets/business/helper/StageCoordinator.java
@@ -23,7 +23,8 @@ public class StageCoordinator {
 
     public static StageCoordinator getInstance() {
         if (instance == null) {
-            return new StageCoordinator();
+               instance = new StageCoordinator();
+               return instance;
         }
 
         return instance;
@@ -66,11 +67,11 @@ public class StageCoordinator {
             SceneData logiSceneData = scenes.get("login");
             primaryStage.setScene(logiSceneData.getScene());
         }
-        primaryStage.setX(80);
-        primaryStage.setY(20);
-        primaryStage.setWidth(1200);
-        primaryStage.setHeight(700);
-        primaryStage.setResizable(false);
+        // primaryStage.setX(80);
+        // primaryStage.setY(20);
+        // primaryStage.setWidth(1200);
+        // primaryStage.setHeight(700);
+        // primaryStage.setResizable(false);
         primaryStage.setTitle("login");
 
     }
@@ -101,7 +102,11 @@ public class StageCoordinator {
             SceneData logiSceneData = scenes.get("signup");
             primaryStage.setScene(logiSceneData.getScene());
         }
-       
+        // primaryStage.setX(80);
+        // primaryStage.setY(20);
+        // primaryStage.setWidth(1200);
+        // primaryStage.setHeight(700);
+        // primaryStage.setResizable(false);
         primaryStage.setTitle("signup");
 
     }
@@ -141,12 +146,12 @@ public class StageCoordinator {
                 // Save file
             });
         }
-        primaryStage.setX(80);
-        primaryStage.setY(20);
-        primaryStage.setWidth(1200);
-        primaryStage.setHeight(700);
-        primaryStage.setResizable(false);
-        primaryStage.setTitle("chat");
+        // primaryStage.setX(80);
+        // primaryStage.setY(20);
+        // primaryStage.setWidth(1200);
+        // primaryStage.setHeight(700);
+        // primaryStage.setResizable(false);
+        // primaryStage.setTitle("chat");
 
     }
     public void moveToCharts() {
